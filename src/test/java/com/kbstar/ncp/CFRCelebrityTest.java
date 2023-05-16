85f3bd0f215ae4d6ea6c7632e6d6bfa14a6d2f23
psm2360440 <psm2360440@gmail.com>
2023-05-16 09:03:37 +0900

    0516

---
+++ src/test/java/com/kbstar/ncp/CFRCelebrityTest.java
@@ -0,0 +1,27 @@
+package com.kbstar.ncp;
+
+import com.kbstar.util.CFRCelebrityUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Slf4j
+@SpringBootTest
+class CFRCelebrityTest {
+
+    @Value("${uploadimgdir}")
+    String imgpath;
+
+
+    @Test
+    void contextLoads() throws Exception{
+        String imgname = "jin2.jpg";
+        JSONObject result = (JSONObject) CFRCelebrityUtil.getResult(imgpath, imgname);
+
+        log.info(result.toJSONString());
+    }
+}
